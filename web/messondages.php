85075d7cf9249a24dd36f7887424f028c210b08a
HyperNovax <florian.lephore@outlook.com>
2018-06-08 15:19:21 +0200

    CSS

--- web/messondages.php
+++ web/messondages.php
@@ -9,22 +9,31 @@
      * On récupère tout les sondages.
      */
     $query = $bdd->query("SELECT * FROM sondage as s WHERE s.dateFin > NOW() and s.dateDebut < NOW() ORDER BY dateFin DESC");
+    ?>
 
-    while($sondage = $query->fetch(PDO::FETCH_ASSOC)) { ?>
+    <div class="container-sondage">
+    <?php while($sondage = $query->fetch(PDO::FETCH_ASSOC)) { ?>
+        <?php
+            $dateDebut  = new DateTime($sondage['dateDebut']);
+            $dateFin = new DateTime($sondage["dateFin"]);
+        ?>
         <div class="row">
-            <div class="col-md-12">
+            <div class="col-md-offset-2 col-md-7">
                 <form action="/web/repondre.php" method="get">
                     <input type="hidden" name="id" value="<?php echo $sondage['id'] ?>">
-                    <div class="row">
-                        <div class="col-md-7">
-                            <?php echo $sondage['titre'] ?>
-                        </div>
-                        <div class="col-md-5">
+                    <div class="row sondage">
+
+                        <div class="col-xs-4 title"><h4><?php echo $sondage['titre'] ?></h4></div>
+                        <div class="col-xs-2"><span class="label label-default"><?php echo $dateDebut->format("d/m/Y") ?></span></div>
+                        <div class="col-xs-2"><span class="label label-default"><?php echo $dateFin->format("d/m/Y") ?></span></div>
+                        <div class="col-xs-4 repondre">
                             <button class="btn btn-primary">Répondre</button>
                         </div>
+
                     </div>
                 </form>
             </div>
         </div>
     <?php } ?>
+    </div>
 
