2d4b2fa85311ce5f2fdb1ec32453f0f8137e19ae
HyperNovax <florian.lephore@outlook.com>
2018-06-20 20:10:29 +0200

    CSS connexion / inscription

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
         if (isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['password']) && !empty($_POST['password'])) {
             $email = $_POST['email'];
             $password = $_POST['password'];
+            $error = "";
 
             $bdd = PdoDatabase::getInstance()->getDbh();
             $sql = "SELECT * FROM utilisateur WHERE email = :email AND password = :password";
@@ -24,6 +25,8 @@
                 $_SESSION['idUser'] = $result['id'];
                 header("Location: /web/messondages.php");
                 exit();
+            } else {
+                $error = "Identifiants incorrect !";
             }
         }
 
@@ -38,36 +41,56 @@
     <title>Sondage</title>
 
     <link rel="stylesheet" href="/css/bootstrap.min.css" />
-    <link rel="stylesheet" href="/css/sondage.css" />
+    <link rel="stylesheet" href="/css/global.css" />
+    <link rel="stylesheet" href="/css/login.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <script src="/js/sortable/Sortable.js"></script>
     <script src="/js/jquery/jquery-3.3.1.min.js"></script>
+    <script src="/js/bootstrap/bootstrap.min.js"></script>
+    <script src="/js/sortable/Sortable.js"></script>
     <script src="/js/sortReponse.js"></script>
 </head>
 
     <body>
         <div class="container">
 
-            <form action="index.php" method="POST">
-                <div class="input-group">
-                    <label for="email">Adresse mail : </label>
-                    <input type="text" name="email" value="" class="form-control">
-                </div>
-                <div class="input-group">
-                    <label for="password">Mot de passe : </label>
-                    <input type="password" name="password" class="form-control">
-                </div>
+            <div class="row">
+                <div class="col-md-offset-4 col-md-4">
+                    <div class="container-form">
 
-                <div class="input-group">
-                    <a href="inscription">Inscription</a>
+                        <div class="title-login">
+                            Sondage <span class="interrogation">?</span>
+                        </div>
+
+                        <form action="index.php" method="POST" class="form-login">
+
+                            <div class="form-group">
+                                <label for="email">Adresse mail : </label>
+                                <input type="text" name="email" value="" class="form-control" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Mot de passe : </label>
+                                <input type="password" name="password" class="form-control" required>
+                            </div>
+
+                            <div class="form-group">
+                                <a href="inscription">Inscription</a>
+                            </div>
+
+                            <div class="form-group">
+                                <input type="submit" name="connexion" value="Connexion" class="btn btn-info btn-lg btn-large btn-connexion">
+                            </div>
+
+                            <?php
+                                if (!empty($error)) {
+                                    echo "<div class='alert alert-danger no-margin alert-dismissible'><a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>".$error."</div>";
+                                }
+                            ?>
+
+                        </form>
+
+                    </div>
                 </div>
-
-                <div class="input-group">
-                    <input type="submit" name="connexion" value="Connexion" class="btn btn-info">
-                </div>
-
-
-            </form>
+            </div>
 
         </div>
     </body>
--- inscription.php
+++ inscription.php
@@ -9,19 +9,38 @@
         if (isset($_POST["email"]) && !empty($_POST["email"]) && isset($_POST["password"]) && !empty($_POST["password"]) && isset($_POST["check_password"]) && !empty($_POST["check_password"])) {
             $email = $_POST['email'];
             $password = $_POST["password"];
+            $check_password = $_POST["check_password"];
             $type = "USER";
+            $error = "";
 
             $bdd = PdoDatabase::getInstance()->getDbh();
-            $sql = "INSERT INTO utilisateur (email, password, type) VALUES(:email, :password, :type)";
+
+            // Vérification de l'adresse email.
+            $sql = "SELECT * FROM utilisateur WHERE email = :email";
             $query = $bdd->prepare($sql);
             $query->bindParam(":email", $email);
-            $query->bindParam(":password", $password);
-            $query->bindParam(":type", $type);
-            $success = $query->execute();
+            $query->execute();
 
-            if ($success) {
-                header("Location: index.php");
-                exit();
+            if (!$query->fetch(PDO::FETCH_ASSOC)) {
+
+                if ($password == $check_password) {
+                    $sql = "INSERT INTO utilisateur (email, password, type) VALUES(:email, :password, :type)";
+                    $query = $bdd->prepare($sql);
+                    $query->bindParam(":email", $email);
+                    $query->bindParam(":password", $password);
+                    $query->bindParam(":type", $type);
+                    $success = $query->execute();
+
+                    if ($success) {
+                        header("Location: index.php");
+                        exit();
+                    }
+                } else {
+                    $error = "Les mots de passe ne sont pas identiques.";
+                }
+
+            } else {
+                $error = "Cette adresse email est déjà utilisé";
             }
 
         }
@@ -36,33 +55,58 @@
         <title>Sondage</title>
 
         <link rel="stylesheet" href="/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="/css/sondage.css" />
+        <link rel="stylesheet" href="/css/global.css" />
+        <link rel="stylesheet" href="/css/login.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     </head>
 
     <body>
 
         <div class="container">
-            <form action="inscription.php" method="POST">
-                <div class="form-group">
-                    <label for="email">Adresse mail :</label>
-                    <input type="text" name="email" class="form-control">
+
+            <div class="row">
+                <div class="col-md-offset-4 col-md-4">
+                    <div class="container-form">
+
+                        <div class="title-login">
+                            Sondage <span class="interrogation">?</span>
+                        </div>
+
+                        <form action="inscription.php" method="POST" class="form-login">
+                            <div class="form-group">
+                                <label for="email">Adresse mail :</label>
+                                <input type="text" name="email" class="form-control" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label form="password">Mot de passe :</label>
+                                <input type="password" name="password" class="form-control" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label form="check_password">Mot de passe :</label>
+                                <input type="password" name="check_password" class="form-control" required>
+                            </div>
+
+                            <div class="form-group">
+                                <input type="submit" name="inscription" value="S'inscrire" class="btn btn-info btn-large btn-lg btn-inscription">
+                            </div>
+
+                            <?php
+                                if (!empty($error)) {
+                                    echo "<div class='alert alert-danger no-margin alert-dismissible'><a href=\"#\" class=\"close\" data-dismiss=\"alert\" aria-label=\"close\">&times;</a>".$error."</div>";
+                                }
+                            ?>
+
+                        </form>
+
+                    </div>
                 </div>
-
-                <div class="form-group">
-                    <label form="password">Mot de passe :</label>
-                    <input type="password" name="password" class="form-control">
-                </div>
-
-                <div class="form-group">
-                    <label form="check_password">Mot de passe :</label>
-                    <input type="password" name="check_password" class="form-control">
-                </div>
-
-                <input type="submit" name="inscription" value="Envoyer">
-
-            </form>
+            </div>
         </div>
 
     </body>
+
+    <script src="/js/jquery/jquery-3.3.1.min.js"></script>
+    <script src="/js/bootstrap/bootstrap.min.js"></script>
 </html>
