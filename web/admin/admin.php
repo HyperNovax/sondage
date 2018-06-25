051f20c1adc9dfc1a43104bb4061a8eb792de56b
HyperNovax <florian.lephore@outlook.com>
2018-06-25 21:49:46 +0200

    Sondage dashboard

--- web/admin/admin.php
+++ web/admin/admin.php
@@ -1,39 +1,52 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Clem
- * Date: 22/05/2018
- * Time: 10:02
- */
 
+    include 'headerAdmin.html';
+    include("../../lib/accesBDD.php");
 
-include 'headerAdmin.html';
-include("../../lib/accesBDD.php");
 ?>
+
 <div class="row">
     <div class="col-md-6">
-        <h1>Les sondages</h1>
-        <br>
-        <ul>
-            <li><a href="newSondage.php">Nouveau sondage</a></li>
-            <?php
+        <div class="dashboard-sondage">
+            <h2>Les sondages</h2>
 
-            /**
-             * On récupère tout les sondages.
-             */
-            $query = $bdd->query("SELECT id, titre, DATE_FORMAT( dateDebut,  '%d/%m/%Y à %H:%i' ) AS dateDebut, DATE_FORMAT( dateFin,  '%d/%m/%Y à %H:%i' ) 
-        AS dateFin FROM sondage as s  ORDER BY dateDebut ASC ");
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Titre</th>
+                        <th>Date de début</th>
+                        <th>Date de fin</th>
+                        <th></th>
+                    </tr>
+                </thead>
 
-            while($sondage = $query->fetch(PDO::FETCH_OBJ)) {
-                echo '<li><a href="statistiquesSondages.php?sondage='.$sondage->id.'">'.$sondage->titre.' du '.$sondage->dateDebut. ' au '. $sondage->dateFin.'</a></li>';
+                <tbody>
+                <?php
+                    /**
+                     * On récupère tout les sondages.
+                     */
+                    $sql = "SELECT id, titre, DATE_FORMAT( dateDebut,  '%d/%m/%Y à %H:%i' ) AS dateDebut, DATE_FORMAT( dateFin,  '%d/%m/%Y à %H:%i' ) AS dateFin FROM sondage as s  ORDER BY dateDebut ASC ";
+                    $query = $bdd->query($sql);
 
+                    while($sondage = $query->fetch(PDO::FETCH_OBJ)) { ?>
+                        <tr>
+                            <td><?php echo $sondage->titre ?></td>
+                            <td><?php echo $sondage->dateDebut ?></td>
+                            <td><?php echo $sondage->dateFin ?></td>
+                            <td><a href="statistiquesSondages.php?sondage=<?php echo $sondage->id ?>" class="btn btn-info btn-repondre">Statistiques</a></td>
+                        </tr>
+                <?php } ?>
+                </tbody>
 
-            }
-            ?>
+            </table>
 
-        </ul>
+            <div class="">
+                <a href="newSondage.php" class="btn btn-success pull-right"><span class="glyphicon glyphicon-plus"></span> Nouveau sondage</a>
+            </div>
 
+        </div>
     </div>
+
     <div class="col-md-6">
 
         <h1>Les utilisateurs</h1>
